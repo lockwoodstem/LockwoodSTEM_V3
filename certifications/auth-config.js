0913c642e25d4b408fdf5e40ccb7208f2e1e1666
lockwoodstem <jonathan@lockwoodstem.org>
2026-07-20 13:08:33 -0400

    456456

--- certifications/auth-config.js
+++ certifications/auth-config.js
@@ -1,9 +1,10 @@
 // LockwoodSTEM Certification Account System
-// Paste your deployed Google Apps Script Web App URL below.
-// Example: https://script.google.com/macros/s/AKfycbw0j9MBMdMG-QNi2IIbp1SE6htXwYgKVV65dV1gLMMTkyK6ujNBWYXtIl-1Jnjlyns/exec
+// Active Google Apps Script Web App deployment.
 window.LOCKWOOD_CERT_AUTH = {
-  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbw0j9MBMdMG-QNi2IIbp1SE6htXwYgKVV65dV1gLMMTkyK6ujNBWYXtIl-1Jnjlyns/exec",
+  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbxg3uyI11W4TYoTE5ZaCbgZWl0ZViItvzCCnfXFt4jgBa9HZs_BH8hk467qhITo6H41/exec",
   REQUIRE_BACKEND: true,
   SESSION_KEY: "lockwoodstem_cert_session",
-  PROFILE_KEY: "lockwoodstem_cert_profile"
+  PROFILE_KEY: "lockwoodstem_cert_profile",
+  TEACHER_SESSION_KEY: "lockwoodstem_teacher_session",
+  TEACHER_PROFILE_KEY: "lockwoodstem_teacher_profile"
 };
